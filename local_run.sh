7a4e5c16acdf80e058fe96e24693b7e6cb009814
dadarakt <janniseichborn@gmail.com>
2020-09-06 12:35:38 +0200

    No hardcoding of script-name

--- local_run.sh
+++ local_run.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
-cat use_fake_gpio.py play_sound.py > mock.py 
-python mock.py
+
+function usage() {
+    echo "Missing or invalid arguments!
+          Usage: $0 `python_script` [python arguments]
+          Where `python_script` is the script to be executed and the arguments 
+          are passed directly to the python execution"
+}
+
+# Exit if no argument is passed in
+if [ $# -le 0 ] 
+then 
+  usage
+  exit 1
+fi 
+
+cat use_fake_gpio.py $1 > mock.py 
+python ${@:2} mock.py
 rm mock.py
